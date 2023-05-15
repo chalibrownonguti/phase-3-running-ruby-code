ffbb82d1b62256b96625712a8c7965daa914ce29
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-15 17:34:38 +0300

    updates

--- app.rb
+++ app.rb
@@ -9,13 +9,13 @@
 # puts [1, 2, 3]
 # p [1, 2, 3]
     # { id: 2, hello: "Ruby" }, 
-    pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+    # pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
 #     { id: 3, hello: "Moon" }, 
 #     { id: 4, hello: "Learner" }]
 
 # p [1,2,3]
 
-# puts "Hello World!"
+puts "Hello World!"
 # print "Pass this test, please."
 # p [1,2,3]
 
